20cadbdc3d64de688dcc7c2efd35f140eb2aa9f7
Alejo Agustín Martinez Aseijas <54213917+AlejoAseijas@users.noreply.github.com>
2022-02-07 17:37:00 -0300

    fix logo

--- src/Components/NavBar/Index.jsx
+++ src/Components/NavBar/Index.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
-import {Nav,Navbar,Container} from "react-bootstrap"
-import logo from "../../assets/logo.jpeg"
+import React from "react";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import logo from "../../assets/logo.jpg";
 function Index() {
-  return <Navbar collapseOnSelect expand="lg" style={{backgroundColor:"rgba(255, 255, 255, 0.3)"}}>
-  <Container>
-  <Navbar.Brand >Salud Avanzada</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link >Quines somos</Nav.Link>
-      <Nav.Link >Principales Prestadores</Nav.Link>
-      <Nav.Link>Contacto</Nav.Link>
-      <Nav.Link>Respaldo</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>;
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      style={{ backgroundColor: "rgba(255, 255, 255, 0.3)" }}
+    >
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo phtho" style={{ width: "10rem" }} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>Quines somos</Nav.Link>
+            <Nav.Link>Principales Prestadores</Nav.Link>
+            <Nav.Link>Contacto</Nav.Link>
+            <Nav.Link>Respaldo</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Index;
