c6a46011bd585801f672c820c82a87d0ca661a96
Alejo Agustín Martinez Aseijas <54213917+AlejoAseijas@users.noreply.github.com>
2022-02-07 20:19:10 -0300

    fix footer

--- src/Components/Footer/Index.jsx
+++ src/Components/Footer/Index.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWhatsapp, faFacebook } from "@fortawesome/free-brands-svg-icons";
-
+import logo from "../../assets/fb-logo.png";
 import "./style.scss";
 function Index() {
   return (
     <footer>
-      <p>
-        © 2022 Salud Avanzada. Todos los derechos reservados. Desarrollado por
-        <a
-          href="https://alejoaseijas.com/"
-          style={{ textDecoration: "none", marginLeft: "5px" }}
-        >
-          Alejo Aseijas
-        </a>
-      </p>
-      <div id="fan-pages">
-        <FontAwesomeIcon
-          icon={faFacebook}
-          style={{ width: "200px" }}
+      <div className="information">
+        <p>
+          © 2022 Salud Avanzada. Todos los derechos reservados. Desarrollado por
+          <a
+            href="https://alejoaseijas.com/"
+            style={{ textDecoration: "none", marginLeft: "5px" }}
+          >
+            Alejo Aseijas
+          </a>
+        </p>
+      </div>
+      <div className="fan-pages">
+        <img
+          src={logo}
+          alt="fb-logo"
           onClick={() => {
             return window.open("https://www.facebook.com/Avanzamos.juntos");
           }}
