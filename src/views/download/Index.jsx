472baf503aba8b35cfadc8ab8826c63f76361777
Alejo Agustín Martinez Aseijas <54213917+AlejoAseijas@users.noreply.github.com>
2022-03-07 00:11:37 -0300

    pdfGeneration

--- src/views/download/Index.jsx
+++ src/views/download/Index.jsx
@@ -1,9 +1,239 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import "./downloader.scss";
+import { PDFExport } from "@progress/kendo-react-pdf";
+import doctoredLogo from "../../assets/providers/doctoredPdf.png";
+import galenoLogo from "../../assets/providers/galenoPdf.png";
+import premedicLogo from "../../assets/providers/premedicPdf.jpg";
 
+import NavBar from "../../Components/NavBar/AdminNavBar";
 function Index() {
+  const dataQuote = JSON.parse(localStorage.getItem("quote"));
+  const pdfExportComponent = useRef(null);
+  const [layoutSelection] = useState({
+    text: "A4",
+    value: "size-a4",
+  });
+
+  const handleExportWithComponent = (event) => {
+    pdfExportComponent.current.save();
+  };
   return (
-    <div>Index</div>
-  )
+    <>
+      <NavBar />
+      {dataQuote.provider === "doctored" ? (
+        <>
+          <div
+            className="page-container hidden-on-narrow"
+            style={{ marginTop: "1em" }}
+          >
+            <PDFExport ref={pdfExportComponent} fileName={dataQuote.plan}>
+              <div className={`pdf-page ${layoutSelection.value}`}>
+                <header>
+                  <div className="pricipal-provider-logo">
+                    <img src={doctoredLogo} alt="photho-logo" />
+                  </div>
+                </header>
+                <div className="tableContainer"></div>
+                <div className="data-information-contact">
+                  <h1>Cotizacion</h1>
+                  <div className="information">
+                    <h4>Dia: {dataQuote.day}</h4>
+                    <h4>Generado por: {dataQuote.operator}</h4>
+                  </div>
+                  <br />
+                  <h3>{dataQuote.planName}</h3>
+
+                  <table className="table" style={{ height: "50px" }}>
+                    <thead>
+                      <tr>
+                        <th scope="col">Item</th>
+                        <th scope="col">Precio</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Cliente edad: {dataQuote.ageClient}</td>
+                        <td>$ {dataQuote.client}</td>
+                      </tr>
+
+                      {dataQuote.patner === 0 ? (
+                        ``
+                      ) : (
+                        <tr>
+                          <td>Conyuge edad: {dataQuote.ageOfSpouse}</td>
+                          <td>$ {dataQuote.patner}</td>
+                        </tr>
+                      )}
+                      {dataQuote.childrens === 0 ? (
+                        ``
+                      ) : (
+                        <tr>
+                          <td>Hijo/s {dataQuote.quantyChildrens}</td>
+                          <td>$ {dataQuote.childrens}</td>
+                        </tr>
+                      )}
+                      {dataQuote.discount === 0 ? (
+                        ``
+                      ) : (
+                        <tr>
+                          <td>Descuento por aporte</td>
+                          <td>-{dataQuote.discount}</td>
+                        </tr>
+                      )}
+                      <tr id="final-price">
+                        <td className="price">Precio final</td>
+                        <td className="price">$ {dataQuote.finalPrice}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="container text-center">
+                    {dataQuote.plan === "DOCTORED1000" ? (
+                      <a href="https://drive.google.com/file/d/1qsTKn2szrE4dtbRwiXfO0PWzAjQaFM9M/view?usp=sharing">
+                        VER CARTILLA
+                      </a>
+                    ) : (
+                      ``
+                    )}
+
+                    {dataQuote.plan === "DOCTORED2000" ? (
+                      <a href="https://drive.google.com/file/d/1axsP31nVKuQQrha5UKs_3n3AVEim1Hc8/view?usp=sharing">
+                        VER CARTILLA
+                      </a>
+                    ) : (
+                      `
+      
+      `
+                    )}
+                    {dataQuote.plan === "DOCTORED3000" ? (
+                      <a href="https://drive.google.com/file/d/1BoB1Y3lg0AGvHd7rT4zyTtW6g3PKtv64/view?usp=sharing">
+                        VER CARTILLA
+                      </a>
+                    ) : (
+                      ` `
+                    )}
+                  </div>
+                </div>
+                <div className="container text-center mt-2">
+                  <img
+                    src="https://i.ibb.co/8s9Bq9z/comparativa.jpg"
+                    style={{ height: "220px", width: "200px" }}
+                    alt="..."
+                  />
+                </div>
+              </div>
+            </PDFExport>
+          </div>
+          <div className="box-col text-center mt-2 mb-2">
+            <button
+              className="btn btn-success"
+              primary={true}
+              onClick={handleExportWithComponent}
+            >
+              Exporta
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div
+            className="page-container hidden-on-narrow"
+            style={{ marginTop: "1em" }}
+          >
+            <PDFExport ref={pdfExportComponent} fileName={dataQuote.plan}>
+              <div className={`pdf-page ${layoutSelection.value}`}>
+                <header>
+                  {dataQuote.provider === "galeno" && (
+                    <div className="pricipal-provider-logo">
+                      <img src={galenoLogo} alt="photho-logo" />
+                    </div>
+                  )}
+                  {dataQuote.provider === "premedic" && (
+                    <div className="pricipal-provider-logo">
+                      <img src={premedicLogo} alt="photho-logo" />
+                    </div>
+                  )}
+                </header>
+                <div className="tableContainer"></div>
+                <div className="data-information-contact">
+                  <h1>Cotizacion</h1>
+                  <div className="information">
+                    <h4>Dia: {dataQuote.day}</h4>
+                    <h4>Generado por: {dataQuote.operator}</h4>
+                  </div>
+                  <br />
+                  <h3>{dataQuote.planName}</h3>
+
+                  <table className="table" style={{ height: "50px" }}>
+                    <thead>
+                      <tr>
+                        <th scope="col">Item</th>
+                        <th scope="col">Precio</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{dataQuote.group}</td>
+                        <td>$ {dataQuote.planPrice}</td>
+                      </tr>
+
+                      {dataQuote.discount === 0 ? (
+                        ``
+                      ) : (
+                        <tr>
+                          <td>Descuento por aporte</td>
+                          <td>-{dataQuote.discount}</td>
+                        </tr>
+                      )}
+                      <tr id="final-price">
+                        <td className="price">Precio final</td>
+                        <td className="price">$ {dataQuote.finalPrice}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="container text-center">
+                    {dataQuote.plan === "DOCTORED1000" ? (
+                      <a href="https://drive.google.com/file/d/1qsTKn2szrE4dtbRwiXfO0PWzAjQaFM9M/view?usp=sharing">
+                        VER CARTILLA
+                      </a>
+                    ) : (
+                      ``
+                    )}
+
+                    {dataQuote.plan === "DOCTORED2000" ? (
+                      <a href="https://drive.google.com/file/d/1axsP31nVKuQQrha5UKs_3n3AVEim1Hc8/view?usp=sharing">
+                        VER CARTILLA
+                      </a>
+                    ) : (
+                      `
+      
+      `
+                    )}
+                    {dataQuote.plan === "DOCTORED3000" ? (
+                      <a href="https://drive.google.com/file/d/1BoB1Y3lg0AGvHd7rT4zyTtW6g3PKtv64/view?usp=sharing">
+                        VER CARTILLA
+                      </a>
+                    ) : (
+                      ` `
+                    )}
+                  </div>
+                </div>
+              </div>
+            </PDFExport>
+          </div>
+
+          <div className="box-col text-center mt-2 mb-2">
+            <button
+              className="btn btn-success"
+              primary={true}
+              onClick={handleExportWithComponent}
+            >
+              Exporta
+            </button>
+          </div>
+        </>
+      )}
+    </>
+  );
 }
 
-export default Index
+export default Index;
